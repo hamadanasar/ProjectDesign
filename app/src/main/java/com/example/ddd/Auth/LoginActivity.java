2c92efd2bc1954af3ae269c0771e894dcc0f0b3e
Hamada <https://github.com/hamadanasar>
2020-10-24 00:43:51 +0200

    sign and login

---
+++ app/src/main/java/com/example/ddd/Auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.ddd.Auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ddd.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/ddd/Auth/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.ddd.Auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ddd.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
